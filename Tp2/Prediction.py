9e2a17f4bb433b80c23a90489eb25cecb66dde55
Martin Kondra <martinkondra@gmail.com>
2019-11-09 16:00:40 -0300

    Initial commit of Prediction class

---
+++ Tp2/Prediction.py
@@ -0,0 +1,52 @@
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+#from sklearn.metrics import mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
+import numpy as np
+#import Inicializacion
+
+data = (pd.read_csv('data/train.csv', index_col=['id'], parse_dates=['fecha'], error_bad_lines=False),
+	    pd.read_csv('data/test.csv', index_col=['id'], parse_dates=['fecha'], error_bad_lines=False))
+
+class Prediction:
+
+    def __init__(self, data, model, param_grid, prefix):
+        self.train_df, self.test_df  = data
+        self.model = model
+        self.param_grid = param_grid
+        self.prefix = prefix
+        self.X =  self.train_df.loc[:,  self.train_df.columns != 'precio']
+        self.y =  self.train_df['precio'].values
+
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X, 
+        	self.y, test_size=0.2, random_state=1)
+
+    def train(self):
+        print('Training...')
+        self.gscv = GridSearchCV(self.model, self.param_grid, scoring='neg_mean_absolute_error', cv=10)
+        self. gscv.fit(self.X, self.y)
+        self.best_params = self.gscv.best_params_
+        self.score = self.gscv.best_score_
+        self.predicted = self.gscv.predict(self.test_df)
+        print(self.best_params)
+        print(self.score)
+
+    def save(self)
+        with open('{}.model'.format(self.prefix),'wb') as f:
+            pickle.dump(self.gscv, f)
+
+    def submit(self):
+        answer = pd.DataFrame(list(zip(test_df.index, predicted)), columns =['id', 'predicted'])
+        answer.to_csv('{}.csv'.format(self.prefix), sep=',', index=False, header=False)
+
+
+if __name__ == '__main__':
+    distances = ['euclidean', 'manhattan', 'chebyshev', 'minkowski']
+    param_grid = {'n_neighbors': np.arange(15, 34, 2), 'metric': distances}
+    model = Prediction(data, KNeighborsRegressor(), param_grid, 'knntest')
+    model.train()
+    model.save()
+    model.submit()
