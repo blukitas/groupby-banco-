1d706638f6b212006c3002a589bb895419a756fa
Martin Kondra <martinkondra@gmail.com>
2019-11-23 16:41:16 -0300

    Typo :(

--- Tp2/ParamsGrid.py
+++ Tp2/ParamsGrid.py
@@ -42,9 +42,9 @@ class ParamsGrid:
         'gbr': (GradientBoostingRegressor(), 
             {'n_estimators':[250], 
             'max_depth':[3],
-            'learning_rate'=[.1, .01, .001]
-            'min_samples_leaf'=[9],
-            'min_samples_split'=[9]})
+            'learning_rate':[.1, .01, .001]
+            'min_samples_leaf':[9],
+            'min_samples_split':[9]})
     }
 
 class singleModels:
