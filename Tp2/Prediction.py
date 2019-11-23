472432a970fe04c4ee3d4c28e67b651921852903
Martin Kondra <martinkondra@gmail.com>
2019-11-23 17:09:24 -0300

    Uncomment

--- Tp2/Prediction.py
+++ Tp2/Prediction.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
-#from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error
 from sklearn.neighbors import KNeighborsRegressor
