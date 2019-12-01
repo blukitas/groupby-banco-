dc06703fb56fd63d6cbf1dfb6481ff7dc45015d2
blukitas <Lucasbertolini@hotmail.com>
2019-12-01 11:45:33 -0300

    CSV pos-fix

--- Tp2/Inicializacion.py
+++ Tp2/Inicializacion.py
@@ -3,16 +3,20 @@
 import os
 import pickle
 from datetime import datetime
+from random import randint
 
 import category_encoders as ce
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif, chi2
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import StratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from xgboost import XGBClassifier, XGBRegressor
 
 
@@ -20,17 +24,42 @@ class Inicializacion:
     # Encoder = 0 #Binario
     # Encoder = 1 #One Hot Encoding
     paramsGenerales = {
-        'verbose': True,  # Agregar mas informacion. Texto, imagenes, guardar.
+        'verbose': False,  # Agregar mas informacion. Texto, imagenes, guardar.
         'encoder': 0,  # Encoder. 0 Binario, 1 One hot encoding
         'esTest': False,  # Si es test no droppeamos nans
         # Para xgboost
+        # 'min_child_weight': [1],
+        # 'gamma': [0.1],
+        # 'subsample': [0.8],
+        # 'colsample_bytree': [0.6],
+        # 'colsample_bynode': [0.6],
+        # 'colsample_bylevel': [0.6],
+        # 'max_depth': [5],
+        # 'n_estimators': [600],
+        # 'learning_rate': [0.08],
         'scoring_regressor': 'neg_mean_squared_error',
         'scoring_cat': 'accuracy',
         'folds': 2,
-        'param_comb': 1,
+        'param_comb': 1
     }
 
-    param_xgboost = {'max_depth': [3], 
+    param_xgboost = {
+        'max_depth': [3],
+        'colsample_bylevel': [1], 
+        'colsample_bynode': [1], 
+        'reg_alpha': [0], 
+        'n_estimators': [100],
+        'colsample_bytree': [1],
+        'learning_rate': [0.1], 
+        'gamma': [0], 
+        'min_child_weight': [1], 
+        'random_state': [42], 
+        'verbosity': [0]
+    }
+
+    '''
+    param_xgboost = {
+        'max_depth': [3],
         'colsample_bylevel': [1], 
         'colsample_bynode': [1], 
         'reg_alpha': [0], 
@@ -41,9 +70,10 @@ class Inicializacion:
         'gamma': [0], 
         'min_child_weight': [1], 
         'random_state': [42], 
-        'verbosity': [0]}
-    '''
-    param_xgboost = {'n_estimators':np.arange(1200,1400,100),
+        'verbosity': [0]
+    }
+    
+    param_xgboost_grid = {'n_estimators':np.arange(1200,1400,100),
         'learning_rate':[0.09,0.10,0.08,0.07],
         'gamma':np.arange(0,1,0.2),
         'colsample_bytree':[0.6],
@@ -66,17 +96,17 @@ class Inicializacion:
         df_test = pd.read_csv('data/test.csv')
 
         # Tiny dataset for debug purposes
-        df = df.sample(frac=0.001)
-        df_test = df_test.sample(frac=0.001)
-        
+        # df = df.sample(frac=0.001)
+        # df_test = df_test.sample(frac=0.001)
+
         self.df_final = self.operaciones(df)
 
         self.paramsGenerales['esTest'] = True
         self.df_final_test = self.operaciones(df_test)
 
         # Salida en csv, para evitar recalculos
-        self.df_final.to_csv('00-df_final.csv',index=False)
-        self.df_final_test.to_csv('01-df_final_test.csv',index=False)
+        self.df_final.to_csv('00-df_final.csv', index=False)
+        self.df_final_test.to_csv('01-df_final_test.csv', index=False)
 
     def getDataframes(self):
         return self.df_final, self.df_final_test
@@ -84,10 +114,13 @@ class Inicializacion:
     def operaciones(self, df):
         print("Comenzando operaciones")
         self.print_len(df)
-        
+
         df = self.casteos(df)
         self.print_len(df)
-        
+
+        df = self.fill_metros(df)
+        self.print_len(df)
+
         df = self.features_engineering(df)
         self.print_len(df)
 
@@ -107,9 +140,9 @@ class Inicializacion:
 
         self.features_engineering_v2(df)
         self.print_len(df)
-        
-        print('Qué columnas tienen nas?')
-        print(df.isna().any())
+
+        # print('Qué columnas tienen nas?')
+        # print(df.isna().any())
 
         # Drop nan que quedaron pos limpieza y tratamientos
         if not self.paramsGenerales['esTest']:
@@ -120,21 +153,28 @@ class Inicializacion:
             df = self.recast(df)
             self.print_len(df)
 
-            # self.metric_selection(df)
-            # self.print_len(df)
+        # self.metric_selection(df)
+        # self.print_len(df)
 
         print('Columnas finales: ')
         print(df.columns)
         self.print_len(df)
-        
+
+        # self.scaler(df)
+        # self.print_len(df)
+
+        # TODO: !! Log en precio y exponencial en la salida.
         return df
 
     def print_len(self, df):
         if self.paramsGenerales['verbose']:
             print("Cantidad de registros: ", len(df))
+            print('Qué columnas tienen nas?')
+            # print(df.isna().any())
+            self.mostrar_nulls(df)
 
-    def casteos(self,df):
-        cols = [x for x in df.columns if x!='precio' and df[x].dtype.type==np.float64]
+    def casteos(self, df):
+        cols = [x for x in df.columns if x != 'precio' and df[x].dtype.type == np.float64]
         for x in cols:
             if df[x].isnull().sum() > 0:
                 pass
@@ -146,7 +186,6 @@ class Inicializacion:
     def tratamiento_nulls(self, df):
         print("   Nulls")
         df = self.drop_cols(df)
-        df = self.fill_metros(df)
         return df
 
     def drop_cols(self, df):
@@ -162,9 +201,9 @@ class Inicializacion:
         print("     Fill metros")
 
         print("       Terreno")
-        cond_mts = df.metroscubiertos>df.metrostotales
-        df['metrostotales'] = np.where(cond_mts,df.metroscubiertos,df.metrostotales)
-        df['metroscubiertos'] = np.where(cond_mts,df.metrostotales,df.metroscubiertos)
+        cond_mts = df.metroscubiertos > df.metrostotales
+        df['metrostotales'] = np.where(cond_mts, df.metroscubiertos, df.metrostotales)
+        df['metroscubiertos'] = np.where(cond_mts, df.metrostotales, df.metroscubiertos)
         df1 = df[df.tipodepropiedad == 'Terreno'].fillna(0)
         df = df[df.tipodepropiedad != 'Terreno']
         df = pd.concat([df, df1])
@@ -189,7 +228,7 @@ class Inicializacion:
         df = df[df.tipodepropiedad != 'Terreno comercial']
 
         df = pd.concat([df, df1])
-        
+
         return df
 
     def predict_nulls(self, df):
@@ -206,7 +245,7 @@ class Inicializacion:
         print("       fill with xgboost. Feature: ", {feature})
 
         # Columnas relevantes (Sin precio)
-        cols = [x for x in df.columns if x != 'precio' and x!='id']
+        cols = [x for x in df.columns if x != 'precio'] # and x!='id']
         # cols = [x for x in df.columns if x not in ['precio', 'titulo', 'tituloh']]
 
         # Todas - la que queremos predecir
@@ -223,14 +262,17 @@ class Inicializacion:
 
         else:
             df_train = df.dropna()
+            df_train.drop(columns=['id']) # Prueba
 
             df_test = df.loc[df[feature].isnull() == True]
+            df_test = (df_test.dropna(subset=cols_subset))  # Prueba
             # Separamos los datos.
             df_train_x = df_train.loc[:, cols_subset]
             df_train_y = df_train[feature]
             df_test_y = df_test[feature]
 
-            df_test_x = df_test
+            # df_test_x = df_test
+            df_test_x = df_test.dropna(subset=cols_subset)
             df_test_x = df_test_x.loc[:, cols_subset]
             df_test = pd.merge(df_test_x, df_test_y.to_frame(), how='inner', left_index=True, right_index=True)
             df_test_x = df_test_x.loc[:, cols_subset]
@@ -248,16 +290,19 @@ class Inicializacion:
             else:
                 xgb = XGBClassifier()
                 scoring = self.paramsGenerales['scoring_cat']
-                cv = skf # 'accuracy'  #
+                # cv = skf  # 'accuracy'  #
+                cv = skf.split(df_train_x, df_train_y)  # 'accuracy'  # Prueba
 
-            random_search = RandomizedSearchCV(xgb, param_distributions=Inicializacion.param_xgboost, n_iter=param_comb, scoring=scoring,
+            random_search = RandomizedSearchCV(xgb,
+                                               # param_distributions=Inicializacion.param_xgboost,
+                                               n_iter=param_comb,
+                                               scoring=scoring,
                                                n_jobs=-1,
                                                cv=cv, random_state=1001)
 
             random_search.fit(df_train_x, df_train_y)
 
             df_test_x[feature + '_xgb'] = random_search.predict(df_test_x)
-
 
         df = pd.merge(df, df_test_x[feature + '_xgb'].to_frame(), how='left', left_index=True, right_index=True)
         df[feature] = np.where((df[feature].isnull()), df[feature + '_xgb'], df[feature])
@@ -322,18 +367,17 @@ class Inicializacion:
                 df = df.join(one_hot_encoded.add_suffix('oh'))
 
         if not self.paramsGenerales['esTest']:
-            self.tiposPrincipales = df.tipodepropiedad.value_counts().sort_values(kind="quicksort", ascending=False).to_frame()
+            self.tiposPrincipales = df.tipodepropiedad.value_counts().sort_values(kind="quicksort",
+                                                                                  ascending=False).to_frame()
             # print(self.tiposPrincipales.head(5).index.tolist())
             self.tiposPrincipales = self.tiposPrincipales.head(5).index.tolist()
-            # print(self.tiposPrincipales)
+        # print(self.tiposPrincipales)
         print('   Encoding top 5 propiedades')
         for tipo in self.tiposPrincipales:
             print('     ' + tipo.replace(' ', '_'))
             df['es_' + tipo.replace(' ', '_')] = df.tipodepropiedad == tipo
 
-        # TODO: Mix encoding
         # TODO: Encoding con promedio
-
         # TODO: Otros encodings?
         # TODO: AUC Scoring?
         return df
@@ -353,26 +397,21 @@ class Inicializacion:
         print("     Ubicacion")
         df['ubicacion'] = df.escuelascercanas + df.centroscomercialescercanos
 
-
         # features basadas en str.contains sobre la descripción
-        zonas_exclusivas = ['Lomas de Chapultepec', 'Polanco', 'Bosques de las Lomas', 'Colinas del Bosque', 'Jardines del Pedregal',
-                   'Lomas Virreyes', 'Tlalpuente', 'Jardines en la Montaña', 'Bosques de Tlalpan']
-        refaccion = ['a reparar', 'a refaccionar','a restaurar']
+        zonas_exclusivas = ['Lomas de Chapultepec', 'Polanco', 'Bosques de las Lomas', 'Colinas del Bosque',
+                            'Jardines del Pedregal',
+                            'Lomas Virreyes', 'Tlalpuente', 'Jardines en la Montaña', 'Bosques de Tlalpan']
+        refaccion = ['a reparar', 'a refaccionar', 'a restaurar']
         lujo = ['marmol', 'mansión', 'lujo', 'jacuzzi']
-        vigilancia = ['vigilancia'] 
+        vigilancia = ['vigilancia']
         country = ['country', 'barrio privado']
-        
+
         print("     Features basadas en la descripción")
         df['zonas_exclusivas'] = df.descripcion.str.contains('|'.join(zonas_exclusivas), na=False, case=False)
         df['refaccion'] = df.descripcion.str.contains('|'.join(refaccion), na=False, case=False)
         df['lujo'] = df.descripcion.str.contains('|'.join(lujo), na=False, case=False)
         df['vigilancia'] = df.descripcion.str.contains('|'.join(vigilancia), na=False, case=False)
         df['country'] = df.descripcion.str.contains('|'.join(country), na=False, case=False)
-
-        # df = df.assign(
-        #     day=df.fecha.dt.day,
-        #     month=df.fecha.dt.month,
-        #     year=df.fecha.dt.year)
 
         df = df.drop(columns='fecha')
 
@@ -386,21 +425,19 @@ class Inicializacion:
         print('nulls en divisor:')
 
         # SACAR CUANDO FIXEEMOS EL XGBOOST que predice ceros...
-        #df.ambientes.replace(0.0, df.ambientes.median(), inplace=True)
-        #df.metroscubiertos.replace(0.0, df.metroscubiertos.median(), inplace=True)
-        #df.metrostotales.replace(0.0, df.metrostotales.median(), inplace=True)
+        # df.ambientes.replace(0.0, df.ambientes.median(), inplace=True)
+        # df.metroscubiertos.replace(0.0, df.metroscubiertos.median(), inplace=True)
+        # df.metrostotales.replace(0.0, df.metrostotales.median(), inplace=True)
 
         print("     tamaño promedio del ambiente")
         df['prom_amb'] = df.metroscubiertos / df.ambientes
         print('     densidad de construccion')
-        df['construccion_density'] = df.metroscubiertos/df.metrostotales
+        df['construccion_density'] = df.metroscubiertos / df.metrostotales
 
         print(df.prom_amb.isnull().values.any())
         print(df.construccion_density.isnull().values.any())
-        
-        return df 
 
-
+        return df
 
     def metric_selection(self, df):
         print("   Metric selection")
@@ -474,7 +511,7 @@ class Inicializacion:
             tmin, tsec = divmod(temp_sec, 60)
             print('       Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
 
-    def mostrar_nulls(self, df, feature):
+    def mostrar_nulls(self, df, feature=randint(1, 101)):
         nulls = pd.DataFrame((df.isnull().sum().sort_values() / len(df) * 100).round(2), columns=['porcentaje de NaN'])
         if (nulls.sum() == 0).bool():
             print("No hay nulls")
@@ -492,11 +529,11 @@ class Inicializacion:
 
             script_dir = os.path.dirname(__file__)
             plots_dir = os.path.join(script_dir, 'plots/')
-            file_name = feature
+            file_name = str(feature)
 
             if not os.path.isdir(plots_dir):
                 os.makedirs(plots_dir)
-
+            plt.show()
             plt.savefig(plots_dir + file_name + '.png')
 
     def get_data_splits(self, df, valid_fraction=0.1):
@@ -510,7 +547,9 @@ class Inicializacion:
 
     def recast(self, df):
         print("   Recast final")
-        cols = [x for x in df.columns if x!='precio' and x!='mes_sin' and x!='mes_cos' and x!='construccion_density' and x!='prom_amb' and df[x].dtype.type == np.float64]
+        cols = [x for x in df.columns if
+                x != 'precio' and x != 'mes_sin' and x != 'mes_cos' and x != 'construccion_density' and x != 'prom_amb' and
+                df[x].dtype.type == np.float64]
         print(cols)
         for x in cols:
             if df[x].isnull().sum() > 0:
@@ -518,19 +557,35 @@ class Inicializacion:
             else:
                 df[x] = df[x].astype(np.int16)
         return df
-        #try:
+        # try:
         #    columns.remove('precio')
         #    columns.remove('mes_sin')
         #    columns.remove('mes_cos')
-        #except:
-            #pass
+        # except:
+        # pass
 
-        #for x in range(len(columns)):
-           # dtype = df[columns[x]].dtype.type
-          #  if dtype == np.int64 or dtype == np.float64:
-         #       df = df.astype({columns[x]: np.int16})
-        #return df
+        # for x in range(len(columns)):
+        # dtype = df[columns[x]].dtype.type
+        #  if dtype == np.int64 or dtype == np.float64:
+        #       df = df.astype({columns[x]: np.int16})
+        # return df
+
+    def scaler(self, df):
+        print("   Scaler")
+
+        # TODO: Distintos scaler, pruebas. Entrenar con train, guardar modelo, si se pudiera y usarlo contra test.
+        # if not self.paramsGenerales['esTest']:
+        #     self.stdScaler = StandardScaler()
+        #     self.stdScaler.fit(df)
+
+        self.stdScaler = StandardScaler()
+        self.stdScaler.fit(df)
+        df = self.stdScaler.transform(df)
+
+        return df
 
 
 if __name__ == '__main__':
     preprocesamiento = Inicializacion()
+
+# TODO: Ensambles: bagging,boosting y stacking son las 3 formas de ensamblar
